778d0278cc9219014a004a61f29cdf39e2f00e16
@fernandogmar <nao125@gmail.com>
2013-01-18 13:22:29 +0100

    upgrade ember to pre.4 and jquery to 1.9.0

--- starter-kit/scripts/config.js
+++ starter-kit/scripts/config.js
@@ -11,11 +11,14 @@ define({
 		'models': 'app/models',
 		'views': 'app/views',
 		'controllers': 'app/controllers',
-    		'templates': 'app/templates',
+    	'templates': 'app/templates',
 		/*libs*/
-		'jquery': 'libs/jquery/1.8.3/jquery',
+		'jquery': 'libs/jquery/1.9.0/jquery',
 		'handlebars': 'libs/handlebars/1.0.rc.1/handlebars',
-		'ember': 'libs/ember/1.0.0-pre2.1/ember'
+		'ember': 'libs/ember/1.0.0-pre.4/ember',
+		/*requirejs-plugins*/
+		'text': 'libs/requirejs-plugins/text',
+		'domReady': 'libs/requirejs-plugins/domReady'
 	}
 }); 
 
--- starter-kit/scripts/main.js
+++ starter-kit/scripts/main.js
@@ -3,8 +3,7 @@
 		requirejs.config(config);
 		require(["App", "domReady!", "ember"], function(App, doc, Ember){
 			var app_name = config.app_name || "App";
-			root[app_name] = App = Ember.Application.create(App);
-			!App.isInitialized && App.initialize();
+			root[app_name] = App = Ember.Application.createWithMixins(App);
 		});
 	});
 })(this);
